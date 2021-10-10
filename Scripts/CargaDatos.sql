c4c6818a6d22cc7f251ace67dc4db2f34099d06e
Hector_Orozco <36681061+hr9457@users.noreply.github.com>
2021-10-10 17:10:04 -0600

    consultas para repartir info en las tablas

--- Scripts/CargaDatos.sql
+++ Scripts/CargaDatos.sql
@@ -4,3 +4,106 @@ INSERT INTO pais(pais.nombre_pais)
 		DISTINCT pais 
 	FROM temporal;
 
+
+--  regiones = 7
+INSERT INTO region(region.nombre_region)    
+	SELECT 
+		DISTINCT temporal.region
+	FROM temporal;
+
+
+-- departamento = 81
+INSERT INTO departamento (departamento.nombre_departamento, departamento.id_region, departamento.id_pais)
+	SELECT
+		DISTINCT temporal.departamento, 
+		region.id_region, 
+		pais.id_pais
+	FROM temporal
+		INNER JOIN pais ON pais.nombre_pais = temporal.pais
+		INNER JOIN region ON region.nombre_region = temporal.region;
+    
+
+
+-- partidos = 18
+INSERT INTO partido (partido.nombre_partido, partido.siglas_partido, partido.id_pais)
+	SELECT
+		DISTINCT temporal.nombre_partido as partido,
+		temporal.partido as siglas,
+		pais.id_pais as pais
+	FROM temporal
+		INNER JOIN pais ON pais.nombre_pais = temporal.pais;
+
+
+
+--  sexo = 2
+INSERT INTO sexo (sexo.nombre_sexo)
+	SELECT
+		DISTINCT temporal.sexo
+	FROM temporal;
+
+
+-- raza = 3
+INSERT INTO raza (raza.nombre_raza)
+	SELECT
+		DISTINCT temporal.raza
+	FROM temporal;
+
+
+-- eleccion = 2
+INSERT INTO eleccion (eleccion.nombre_eleccion, eleccion.anio_eleccion) 
+	SELECT
+		DISTINCT temporal.nombre_eleccion,
+		temporal.anio_eleccion
+	FROM temporal;
+
+
+-- municipios = 1170 
+INSERT INTO municipio (municipio.nombre_municipio, municipio.id_departamento)
+	SELECT 
+		DISTINCT temporal.municipio,
+		departamento.id_departamento
+	FROM temporal
+		INNER JOIN departamento ON departamento.nombre_departamento = temporal.departamento;
+
+
+
+
+
+-- votaciones = 20970 
+INSERT INTO votacion (votacion.analfabetos, votacion.alfabetos, 
+						votacion.primaria, votacion.educacion_media, 
+                        votacion.universitario, votacion.id_raza, votacion.id_sexo,
+                        votacion.id_municipio, votacion.id_partido, votacion.id_eleccion)
+	SELECT
+		temporal.analfabetos,
+		temporal.alfabetos,
+		temporal.primaria,
+		temporal.nivel_medio,
+		temporal.universitario,
+        raza.id_raza,
+		sexo.id_sexo,	
+        municipio.id_municipio,
+        partido.id_partido,
+		eleccion.id_eleccion	
+	FROM temporal
+		INNER JOIN sexo ON sexo.nombre_sexo = temporal.sexo
+		INNER JOIN raza ON raza.nombre_raza = temporal.raza
+		INNER JOIN eleccion ON eleccion.anio_eleccion = temporal.anio_eleccion
+        INNER JOIN partido ON partido.nombre_partido = temporal.nombre_partido AND partido.siglas_partido = temporal.partido
+		INNER JOIN municipio ON municipio.nombre_municipio = temporal.municipio 
+		INNER JOIN departamento ON departamento.id_departamento = municipio.id_departamento
+		INNER JOIN pais ON pais.id_pais = departamento.id_pais
+		WHERE temporal.departamento = departamento.nombre_departamento;
+
+
+
+
+-- consulta para hacer conteos de las de las consultas
+SELECT 
+	COUNT(*)
+	FROM(
+		SELECT
+		DISTINCT temporal.departamento, pais.id_pais
+		FROM temporal
+		INNER JOIN pais ON pais.nombre_pais = temporal.pais
+    ) ciudades;
--- Scripts/ScriptCreacion.sql
+++ Scripts/ScriptCreacion.sql
@@ -3,19 +3,19 @@ use Proyecto;
 -- tabla de pais
 CREATE TABLE pais(
 	id_pais INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_pais VARCHAR(100)
+    nombre_pais VARCHAR(100) NOT NULL
 );
 
 -- tabla de regiones
 CREATE TABLE region(
 	id_region INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_region VARCHAR(100)
+    nombre_region VARCHAR(100) NOT NULL
 );
 
 -- tabla de departamentos
 CREATE TABLE departamento(
 	id_departamento INT NOT NULL AUTO_INCREMENT,
-    nombre_departamento VARCHAR(100),
+    nombre_departamento VARCHAR(100) NOT NULL,
     id_region INT NOT NULL,
     id_pais INT NOT NULL,
     PRIMARY KEY (id_departamento,id_region,id_pais),
@@ -27,8 +27,8 @@ CREATE TABLE departamento(
 -- tabla para partidos politicos
 CREATE TABLE partido(
     id_partido INT NOT NULL AUTO_INCREMENT,
-    nombre_partido VARCHAR(100),
-    siglas_partido VARCHAR(100),
+    nombre_partido VARCHAR(100) NOT NULL,
+    siglas_partido VARCHAR(100) NOT NULL,
     id_pais INT NOT NULL,
     PRIMARY KEY (id_partido,id_pais),
     FOREIGN KEY (id_pais) REFERENCES pais (id_pais)
@@ -37,7 +37,7 @@ CREATE TABLE partido(
 -- tabla para municipios
 CREATE TABLE municipio(
     id_municipio INT NOT NULL AUTO_INCREMENT,
-    nombre_municipio VARCHAR(100),
+    nombre_municipio VARCHAR(100) NOT NULL,
     id_departamento INT NOT NULL,
     PRIMARY KEY (id_municipio,id_departamento),
     FOREIGN KEY (id_departamento) REFERENCES departamento (id_departamento)
@@ -47,34 +47,44 @@ CREATE TABLE municipio(
 -- tabla para raza
 CREATE TABLE raza(
     id_raza INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_raza VARCHAR(50)
+    nombre_raza VARCHAR(50) NOT NULL
 );
 
 
 -- tabla para sexo de las personas
 CREATE TABLE sexo(
     id_sexo INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nombre_sexo VARCHAR(50)
+    nombre_sexo VARCHAR(50) NOT NULL
+);
+
+
+
+-- talba para tipo eleccion por anio
+CREATE TABLE eleccion(
+    id_eleccion INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombre_eleccion VARCHAR(100) NOT NULL,
+    anio_eleccion INT NOT NULL
 );
 
 
 -- tabla para votaciones
-CREATE TABLE votacion(
+CREATE TABLE votacion(    
+    analfabetos INT NOT NULL,
+    alfabetos INT NOT NULL,
+    primaria INT NOT NULL,
+    educacion_media INT NOT NULL,
+    universitario INT NOT NULL,
     id_raza INT NOT NULL,
     id_sexo INT NOT NULL,
     id_municipio INT NOT NULL,
     id_partido INT NOT NULL,
-    analfabetos INT NOT NULL,
-    alfabetos INT NOT NULL,
-    primaria INT NOT NULL,
-    educacion_meia INT NOT NULL,
-    universitario INT NOT NULL,
-    anio_votacion INT NOT NULL,
-    PRIMARY KEY (id_raza,id_sexo,id_municipio,id_partido),
+    id_eleccion INT NOT NULL,
+    PRIMARY KEY (id_raza,id_sexo,id_municipio,id_partido,id_eleccion),
     FOREIGN KEY (id_raza) REFERENCES raza(id_raza),
     FOREIGN KEY (id_sexo) REFERENCES sexo(id_sexo),
     FOREIGN KEY (id_municipio) REFERENCES municipio(id_municipio),
-    FOREIGN KEY (id_partido) REFERENCES partido(id_partido)
+    FOREIGN KEY (id_partido) REFERENCES partido(id_partido),
+    FOREIGN KEY (id_eleccion) REFERENCES eleccion(id_eleccion)
 );
 
 
@@ -84,6 +94,7 @@ CREATE TABLE votacion(
 -- *************************************
 DROP TABLE votacion; 
 DROP TABLE raza;
+DROP TABLE eleccion;
 DROP TABLE sexo;
 DROP TABLE municipio;  
 DROP TABLE departamento;
